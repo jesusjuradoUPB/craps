981226d05cc7d7e4b4f657752f935c5d96a69aae
jesusjurado <jesus.jurado@udea.edu.co>
2021-11-12 23:20:50 -0500

    archivo js y guardado de los valores de cada tiro

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
-window.onload = inicio;
-function inicio(){
-	alert("Bienvenido al juego de craps ¡mucha suerte!");
+var posiciones = [0,-160,-320,-481,-642,-803];
+var dado1,dado2,boton_tirar;
+
+window.onload = init;
+function init(){
+  dado1 = document.getElementById("dado1");
+  dado2 = document.getElementById("dado2");
+  boton_tirar = document.getElementById("boton_tirar");
+  boton_tirar.addEventListener("click",jugar);
+}
+
+//https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+function tirardado(ref){
+  var cara = Math.floor(Math.random() * 6) + 1 ;
+  ref.style.backgroundPosition = posiciones[cara-1]+"px";
+  return cara;
+}
+
+function jugar(){
+  let tiro1 = tirardado(dado1);
+  let tiro2 = tirardado(dado2);
+
+  console.log("Tiro 1: "+tiro1)
+  console.log("Tiro 2: "+tiro2)
+
 }
